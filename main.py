46b8eaa7fe2a08ca7cf9e25e8550b23cef2de6a8
weitnow <weitnow@gmail.com>
2023-03-23 20:32:38 +0100

    skeleton implemented

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
-style = ttk.Style(root)
+class PomodoroTimer(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
+        self.title("Pomodoro Timer")
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
 
-name = ttk.Label(root, text="Hello, world")
-name.pack()
+        container = ttk.Frame(self)
+        container.grid()
+        container.columnconfigure(0, weight=1)
 
-style.theme_use("clam")
+        timer_frame = Timer(container)
+        timer_frame.grid(row=0, column=0, sticky="NESW")
 
-style.configure("TLabel", bordercolor="#f00")
-style.configure("TLabel", borderwidth=20)
-style.configure("TLabel", relief="solid")
+class Timer(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
 
-print(style.layout("TLabel"))
+        self.current_time = tk.StringVar(value="00:10")
+        self.timer_running = True
 
-root.mainloop()
+        timer_frame = ttk.Frame(self, height="100")
+        timer_frame.grid(pady=(10, 0), sticky="NSEW")
+
+        timer_counter = ttk.Label(
+            timer_frame,
+            textvariable=self.current_time
+        )
+        timer_counter.grid()
+
+        self.decrement_time()
+
+    def decrement_time(self):
+        pass
+
+app = PomodoroTimer()
+app.mainloop()
